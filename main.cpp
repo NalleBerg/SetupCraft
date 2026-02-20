2d4536038813a7c54d5e07601e73d24eb3543c06
Nalle Berg <Nalle@nalle.no>
2026-02-20 13:15:34 +0200

    Persist selected language to AppData\SetupCraft\SetupCraft.ini and restore on startup

--- main.cpp
+++ main.cpp
@@ -61,6 +61,64 @@ static void NormalizeDisplayName(std::wstring &s) {
     }
 }
 
+static std::wstring GetAppDataDir() {
+    wchar_t buf[MAX_PATH];
+    DWORD len = GetEnvironmentVariableW(L"APPDATA", buf, _countof(buf));
+    if (len == 0 || len > _countof(buf)) return L"";
+    std::wstring path(buf);
+    path += L"\\SetupCraft";
+    DWORD attrs = GetFileAttributesW(path.c_str());
+    if (attrs == INVALID_FILE_ATTRIBUTES) {
+        // try to create (ignore failures)
+        CreateDirectoryW(path.c_str(), NULL);
+    }
+    return path;
+}
+
+static bool ReadSavedLocale(std::wstring &outCode) {
+    outCode.clear();
+    std::wstring appdir = GetAppDataDir();
+    if (appdir.empty()) return false;
+    std::wstring ini = appdir + L"\\SetupCraft.ini";
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, ini.c_str(), -1, NULL, 0, NULL, NULL);
+    if (size_needed <= 0) return false;
+    std::string narrow(size_needed - 1, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, ini.c_str(), -1, &narrow[0], size_needed, NULL, NULL);
+    std::ifstream f(narrow);
+    if (!f.is_open()) return false;
+    std::string line;
+    while (std::getline(f, line)) {
+        if (line.empty() || line[0] == '#') continue;
+        auto pos = line.find('=');
+        if (pos == std::string::npos) continue;
+        std::string key = line.substr(0, pos);
+        std::string val = line.substr(pos + 1);
+        auto trim = [](std::string &s){ size_t a = s.find_first_not_of(" \t\r\n"); if (a==std::string::npos) { s.clear(); return; } size_t b = s.find_last_not_of(" \t\r\n"); s = s.substr(a, b - a + 1); };
+        trim(key); trim(val);
+        if (key == "language" || key == "lang") { outCode = Utf8ToW(val); return true; }
+    }
+    return false;
+}
+
+static void WriteSavedLocale(const std::wstring &code) {
+    std::wstring appdir = GetAppDataDir();
+    if (appdir.empty()) return;
+    std::wstring ini = appdir + L"\\SetupCraft.ini";
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, ini.c_str(), -1, NULL, 0, NULL, NULL);
+    if (size_needed <= 0) return;
+    std::string narrow(size_needed - 1, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, ini.c_str(), -1, &narrow[0], size_needed, NULL, NULL);
+    std::ofstream f(narrow, std::ios::trunc);
+    if (!f.is_open()) return;
+    int sz = WideCharToMultiByte(CP_UTF8, 0, code.c_str(), -1, NULL, 0, NULL, NULL);
+    std::string codeUtf8;
+    if (sz > 0) {
+        codeUtf8.resize(sz - 1);
+        WideCharToMultiByte(CP_UTF8, 0, code.c_str(), -1, &codeUtf8[0], sz, NULL, NULL);
+    }
+    f << "language=" << codeUtf8 << "\n";
+}
+
 static bool LoadLocaleFile(const std::wstring &code, std::map<std::wstring, std::wstring> &out) {
     wchar_t path[MAX_PATH];
     swprintf_s(path, L"locale\\%s.txt", code.c_str());
@@ -202,7 +260,40 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             if (itn != displayName.end()) name = itn->second; else name = code;
             LRESULT idx = SendMessageW(hCombo, CB_ADDSTRING, 0, (LPARAM)name.c_str());
             SendMessageW(hCombo, CB_SETITEMDATA, (WPARAM)idx, (LPARAM)i);
-            if (code == L"en_GB") SendMessageW(hCombo, CB_SETCURSEL, (WPARAM)idx, 0);
+        }
+
+        // If user has previously selected a language, read it from AppData and set selection
+        std::wstring savedLocale;
+        if (ReadSavedLocale(savedLocale) && !savedLocale.empty()) {
+            auto it = std::find(g_availableLocales.begin(), g_availableLocales.end(), savedLocale);
+            if (it != g_availableLocales.end()) {
+                int localeIdx = (int)std::distance(g_availableLocales.begin(), it);
+                int count = (int)SendMessageW(hCombo, CB_GETCOUNT, 0, 0);
+                for (int ci = 0; ci < count; ++ci) {
+                    LRESULT itemData = SendMessageW(hCombo, CB_GETITEMDATA, ci, 0);
+                    if ((int)itemData == localeIdx) {
+                        SendMessageW(hCombo, CB_SETCURSEL, ci, 0);
+                        // load that locale and update UI strings
+                        if (LoadLocaleFile(savedLocale, g_locale)) {
+                            auto itName = g_locale.find(L"app_name");
+                            if (itName != g_locale.end()) SetWindowTextW(hwnd, itName->second.c_str());
+                            auto itOk = g_locale.find(L"ok");
+                            if (itOk != g_locale.end()) SetWindowTextW(GetDlgItem(hwnd, IDOK), itOk->second.c_str());
+                        }
+                        break;
+                    }
+                }
+            }
+        } else {
+            // default to en_GB if available
+            for (int idx = 0; idx < (int)SendMessageW(hCombo, CB_GETCOUNT, 0, 0); ++idx) {
+                LRESULT itemData = SendMessageW(hCombo, CB_GETITEMDATA, idx, 0);
+                int li = (int)itemData;
+                if (li >= 0 && li < (int)g_availableLocales.size() && g_availableLocales[li] == L"en_GB") {
+                    SendMessageW(hCombo, CB_SETCURSEL, idx, 0);
+                    break;
+                }
+            }
         }
 
         // OK button (positioned to the right of the combo)
@@ -238,6 +329,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                         if (itName != g_locale.end()) SetWindowTextW(hwnd, itName->second.c_str());
                         auto itOk = g_locale.find(L"ok");
                         if (itOk != g_locale.end()) SetWindowTextW(GetDlgItem(hwnd, IDOK), itOk->second.c_str());
+                        // persist selection to AppData
+                        WriteSavedLocale(code);
                     }
                 }
             }
